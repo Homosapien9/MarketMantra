38ff4b332cc45a8e99b3e278d91b5a67bae16a74
Homosapien9 <peacelord1309@gmail.com>
2024-11-18 23:02:31 +0530

    Update x.py

--- x.py
+++ x.py
@@ -284,8 +284,7 @@ latest_data = df.iloc[-1:][['Previous Close', 'Daily Return']].values.reshape(1,
 latest_data_scaled = scaler.transform(latest_data)
 predicted_trend = models[selected_model].predict(latest_data_scaled)
 
-#defining tabs
-tab1, tab2, tab3, tab4 , tab5 = st.tabs(["Portfolio", "Watchlist", "Technical indicators", "predictions", "News"])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Portfolio", "Watchlist", "Technical indicators", "Predictions", "News"])
 
 # Initialize portfolio and watchlist in session_state if they do not exist
 if 'portfolio' not in st.session_state:
